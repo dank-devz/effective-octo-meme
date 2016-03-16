a4b073882db907669be622de964a0856c8fa87f3
dank-developer <Erikdkarlsson8@gmail.com>
2016-03-15 22:52:35 -0700

    Added a darkgreen stylesheet.

--- app/main.cpp
+++ app/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile file(":/qss/darkorange.qss");
+    QFile file(":/qss/darkgreen.qss");
        if(file.open(QIODevice::ReadOnly | QIODevice::Text))
        {
            a.setStyleSheet(file.readAll());
